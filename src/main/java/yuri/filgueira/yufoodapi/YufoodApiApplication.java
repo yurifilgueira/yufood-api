f2ac443794921e6e019e7ee75a16bf968fb98a57
Yuri FTM <yurimedeiros141@gmail.com>
2024-04-20 16:50:31 -0300

    Initial commit

---
+++ src/main/java/yuri/filgueira/yufoodapi/YufoodApiApplication.java
@@ -0,0 +1,13 @@
+package yuri.filgueira.yufoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YufoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YufoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yuri/filgueira/yufoodapi/YufoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package yuri.filgueira.yufoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YufoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
