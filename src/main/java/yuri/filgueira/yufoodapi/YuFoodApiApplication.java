2c17e522c83ee652d2826f908e91914d7ef2006f
Yuri Filgueira <yurimedeiros141@gmail.com>
2024-05-19 12:39:27 -0300

    The Mockito dependency was added to the project
    The Mockito dependency was added for testing purposes, which will be implemented soon.

--- src/main/java/yuri/filgueira/yufoodapi/YuFoodApiApplication.java
+++ src/main/java/yuri/filgueira/yufoodapi/YuFoodApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YufoodApiApplication {
+public class YuFoodApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YufoodApiApplication.class, args);
+		SpringApplication.run(YuFoodApiApplication.class, args);
 	}
 
 }
