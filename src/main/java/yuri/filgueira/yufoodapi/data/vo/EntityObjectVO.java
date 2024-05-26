9c6cc70273fb56f66731728a8aac374216d28a6d
Yuri Filgueira <yurimedeiros141@gmail.com>
2024-05-26 12:25:57 -0300

    EntityObjectVO created

---
+++ src/main/java/yuri/filgueira/yufoodapi/data/vo/EntityObjectVO.java
@@ -0,0 +1,81 @@
+package yuri.filgueira.yufoodapi.data.vo;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public abstract class EntityObjectVO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    protected Long key;
+    protected String name;
+    protected String email;
+
+    private Set<AddressVO> addresses =  new HashSet<>();
+
+    public EntityObjectVO() {
+    }
+
+    public EntityObjectVO(Long key, String name, String email, Set<AddressVO> addresses) {
+        this.key = key;
+        this.name = name;
+        this.email = email;
+        this.addresses = addresses;
+    }
+
+    public Long getKey() {
+        return key;
+    }
+
+    public void setKey(Long key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<AddressVO> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<AddressVO> addresses) {
+        this.addresses = addresses;
+    }
+
+    public void addAddress(AddressVO address) {
+        addresses.add(address);
+    }
+
+    public void removeAddress(AddressVO address) {
+        addresses.remove(address);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EntityObjectVO that)) return false;
+        return Objects.equals(getKey(), that.getKey());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getKey());
+    }
+}
--- src/main/java/yuri/filgueira/yufoodapi/mapper/modelMapper/config/ModelMapperConfig.java
+++ src/main/java/yuri/filgueira/yufoodapi/mapper/modelMapper/config/ModelMapperConfig.java
@@ -3,8 +3,12 @@ package yuri.filgueira.yufoodapi.mapper.modelMapper.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import yuri.filgueira.yufoodapi.data.vo.AddressVO;
+import yuri.filgueira.yufoodapi.data.vo.EntityObjectVO;
 import yuri.filgueira.yufoodapi.data.vo.FoodVO;
 import yuri.filgueira.yufoodapi.data.vo.OrderItemVO;
+import yuri.filgueira.yufoodapi.entities.Address;
+import yuri.filgueira.yufoodapi.entities.EntityObject;
 import yuri.filgueira.yufoodapi.entities.Food;
 import yuri.filgueira.yufoodapi.entities.OrderItem;
 
@@ -26,6 +30,18 @@ public class ModelMapperConfig {
 
         modelMapper.createTypeMap(OrderItemVO.class, OrderItem.class)
                 .addMapping(OrderItemVO::getKey, OrderItem::setId);
+
+        modelMapper.createTypeMap(Address.class, AddressVO.class)
+                .addMapping(Address::getId, AddressVO::setKey);
+
+        modelMapper.createTypeMap(AddressVO.class, Address.class)
+                .addMapping(AddressVO::getKey, Address::setId);
+
+        modelMapper.createTypeMap(EntityObject.class, EntityObjectVO.class)
+                .addMapping(EntityObject::getId, EntityObjectVO::setKey);
+
+        modelMapper.createTypeMap(EntityObjectVO.class, EntityObject.class)
+                .addMapping(EntityObjectVO::getKey, EntityObject::setId);
 
         return modelMapper;
     }
